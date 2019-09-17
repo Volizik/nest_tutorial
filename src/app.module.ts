e15e92444c99a851a70d2c44628ca42c1f8b3e14
Volizik <kizilov.yura@gmail.com>
2019-09-17 23:44:59 +0300

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {TypeOrmConfig} from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
       TasksModule,
+      AuthModule,
       TypeOrmModule.forRoot(TypeOrmConfig),
   ],
   controllers: [AppController],
